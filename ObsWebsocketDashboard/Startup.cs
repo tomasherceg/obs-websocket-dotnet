898f8c92e5659cbe86bc2cb07b9d92b20060b131
Tomáš Herceg <tomas.herceg@riganti.cz>
2019-09-22 15:19:15 +0200

    Client ID made configurable

--- ObsWebsocketDashboard/Startup.cs
+++ ObsWebsocketDashboard/Startup.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using ObsWebsocketDashboard.Hubs;
@@ -14,6 +17,18 @@ namespace ObsWebsocketDashboard
 {
     public class Startup
     {
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile(Path.Combine(env.ContentRootPath, "appsettings.json"))
+                .AddUserSecrets(typeof(Startup).Assembly)
+                .Build();
+        }
+
+        public static IConfigurationRoot Configuration { get; set; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
